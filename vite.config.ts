78319ea7f995fab90bf7e9685f8255299a03f74f
Dmitry Matyshev <deemoor4@gmail.com>
2026-08-19 20:29:02 +0400

    fix: updated gh-pages path

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import path from 'path'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/staff-page/',
+  base: '/test-task-staff/',
   resolve: {
     alias: {
       'src': path.resolve(__dirname, './src'),
